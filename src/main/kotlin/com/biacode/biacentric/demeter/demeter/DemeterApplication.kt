ffc55094e4d35337abe93890637317c1fad1b16e
Arthur Asatryan <biacoder@gmail.com>
2017-11-06 16:41:22 +0400

    Apply #1 changes.

---
+++ src/main/kotlin/com/biacode/biacentric/demeter/demeter/DemeterApplication.kt
@@ -0,0 +1,11 @@
+package com.biacode.biacentric.demeter.demeter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemeterApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemeterApplication>(*args)
+}
